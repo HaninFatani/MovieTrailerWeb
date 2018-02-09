dd4611dbb625fff910e4096caae77b944d85f112
HaninFatani <norma.chan94@gmail.com>
2018-02-09 18:11:31 +0300

    Add files via upload

---
+++ entertainment_center.py
@@ -0,0 +1,40 @@
+import media
+import fresh_tomatoes
+
+
+naruto = media.Movie("The Last: Naruto the Movie",
+                     "1:30 hour",
+                     "two years since the 4th Ninja World War, another threat surfaces!",
+                     "https://upload.wikimedia.org/wikipedia/en/0/0c/TheLastNarutomovie.jpg",
+                     "https://www.youtube.com/watch?v=tA3yE4_t6SY")
+
+
+spritied_away = media.Movie("Siprited Away",
+                            "2:00 hours",
+                            "Stubborn and spoiled 10-year-old Chihiro Ogino where strange things occured to her and her parents in their way to a new house",
+                            "https://vignette.wikia.nocookie.net/studio-ghibli/images/4/4a/Spirited_Away_%28Amerikansk_DVD%29.jpg/revision/latest?cb=20140116135457",
+                            "https://www.youtube.com/watch?v=ByXuk9QqQkk")
+
+
+black_butler = media.Movie("Black Butler: Book of Atlantic",
+                           "1:00 hour",
+                           "The Earl Phantomhive and his butler Sebastian face another mysterious case on a ship",
+                           "https://myanimelist.cdn-dena.com/images/anime/9/85792.jpg",
+                           "https://www.youtube.com/watch?v=YFRGd6DMtjs")
+
+
+monester_university = media.Movie("Monester University",
+                                  "1:45 hour",
+                                  "Mike Wazowski (Billy Crystal) has dreamed of becoming a Scarer. To make his dream a reality, he enrolls at Monsters University",
+                                  "http://t1.gstatic.com/images?q=tbn:ANd9GcRwIJWk6sSJtE5FNXg1pK4lFu5tbeIvi81BKV2SG4wTA1lWfrMH",
+                                  "https://www.youtube.com/watch?v=QxrQ6BaijAY")
+
+big_hero_6 = media.Movie("Big Hero 6",
+                         "1:30 hour",
+                         "Robotics prodigy Hiro (Ryan Potter) lives in the city of San Fransokyo. Next to his older brother, Tadashi, Hiro's closest companion is Baymax (Scott Adsit), a robot whose sole purpose is to take care of people. When a devastating turn of events throws Hiro into the middle of a dangerous plot,",
+                         "http://t2.gstatic.com/images?q=tbn:ANd9GcQzyu98HxFhB68UKqRKSrTKknXHI-gtSTAAX0CGiKBM980CFhI1",
+                         "https://www.youtube.com/watch?v=z3biFxZIJOQ")
+
+
+movies = [naruto, spritied_away, black_butler, monester_university, big_hero_6]
+fresh_tomatoes.open_movies_page(movies)
---
+++ media.py
@@ -0,0 +1,26 @@
+import webbrowser
+
+
+class Video():
+
+    """A parent class, so the developer can inherit from"""
+    
+    def __init__(self, title, duration):
+        self.title = title
+        self.duration = duration
+
+
+class Movie(Video):
+
+    """Class to store movies in order to run them in website"""
+
+    def __init__(self, title, duration, storyline, img, trailer):
+        Video.__init__(self, title, duration)
+        self.title = title
+        self.storyline = storyline
+        self.poster_image_url = img
+        self.trailer_youtube_url = trailer
+
+        #to run the trailer videos in the website
+    def show_trailer(self):
+        webbrowser.open(self.trailer_youtube_url)
